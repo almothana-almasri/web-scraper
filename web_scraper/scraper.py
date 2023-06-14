56db2c2187ce469127d2765388707b070834d25e
almothana-almasri <mothana.almasri.99@gmail.com>
2023-06-14 18:14:00 +0000

    Added docString

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,16 +1,34 @@
 import requests
-from  bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 def get_citations_needed_count(URL):
+    """
+    Retrieves the number of citations needed for a given Wikipedia page.
+
+    Args:
+        URL (str): The URL of the Wikipedia page.
+
+    Returns:
+        int: The count of citations needed.
+    """
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser')
-    all_citation_needed = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")
-    print (len(all_citation_needed))
+    all_citation_needed = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
+    print(len(all_citation_needed))
     return len(all_citation_needed)
 
 get_citations_needed_count('https://en.wikipedia.org/wiki/Anglo-Iraqi_War')
 
 def get_citations_needed_report(URL):
+    """
+    Generates a report of the passages that require citations for a given Wikipedia page.
+
+    Args:
+        URL (str): The URL of the Wikipedia page.
+
+    Returns:
+        str: The report string containing the relevant passages.
+    """
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
     all_citation_needed = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
@@ -18,7 +36,7 @@ def get_citations_needed_report(URL):
     for citation in all_citation_needed:
         parent_text = citation.find_parent('p').text.strip()
         report += f"Citation needed for: {parent_text}\n\n"
-    print (report)
+    print(report)
     return report
 
 get_citations_needed_report('https://en.wikipedia.org/wiki/Anglo-Iraqi_War')
